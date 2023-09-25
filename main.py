394909cb4609ea0475f9220f3f3f40ac9efe066e
Arnaud Rachez <arnaud@linalgo.com>
2023-09-25 17:10:41 +0900

    feat: refactor to use NPV

--- main.py
+++ main.py
@@ -12,128 +12,162 @@ from models import Bank, Loan, RealEstate, Rent, Salary
 n = st.sidebar.slider("Timeline (years)", value=35, max_value=50)
 
 st.sidebar.markdown('# Economic conditions')
-market_rate = st.sidebar.number_input('Stock Market return', value=0.05, format='%f', help='This return is used for inveting any cash left after paying the rent and/or loan into the stock market.')
-appreciation_rate = st.sidebar.number_input('House Market return', value=0.02, format='%f', key=10, help='This return is used on the house and land.')
-rent_increase_rate = st.sidebar.number_input('Rent increase rate', value=0.01, help='The yearly rate at which the rent increases.')
-inflation_rate = st.sidebar.number_input('Inflation rate', value=0.02, format='%f', help='The inflation will not affect the value of available cash to invest after all payments. However, it will affect the value of the net worth. Setting the inflation rate to zero will make the Net Value be nominal, anything else will show real net worth.')
-monthly_salary = st.sidebar.number_input('Monthly budget (万円)', value=32, help='This the amount of cash available to pay the rent or loan. If some cash is left after the rent or loan is paid, it will be invested in the stock market.')
-starting_balance = st.sidebar.number_input('Starting bank balance (万円)', value=0, help='This is the balance of your bank account before you buy a house or start renting. This balance can be used to pay the first down payment on the house. Otherwise, it will be invested in the stock market entirely.')
-salary_increase_rate = st.sidebar.number_input('Monthly budget increase rate', value=0.0, format='%f', help=' The yearly rate at which you increase your budget.')
-year_until_retirement = st.sidebar.number_input('Years until retirement', value=30, help='After you retire, your budget will be automatically set to zero.')
+appreciation_rate = st.sidebar.number_input(
+    'Housing Market return', 
+    value=0.02, 
+    format='%f',
+    help='This return is used on the house and land.'
+)
+inflation_rate = st.sidebar.number_input(
+    'Inflation rate', 
+    value=0.02, 
+    format='%f',
+    help='Used to discount the future value of future payments. It will affect the real NPV of the project.'
+)
+invest_difference = st.sidebar.checkbox(
+    "Invest difference in the stock market", 
+    value=False, 
+    help="Each year one project has a cost bigger than the other, the second project will invest the difference in the stock market."
+)
+market_rate = 0
+if invest_difference:
+    market_rate = st.sidebar.number_input(
+        'Stock Market return', 
+        value=0.05, 
+        format='%f',
+        help='This return is used for inveting any cash left after paying the rent and/or loan into the stock market.'
+    )
 
 tabs = st.tabs(["Summary", "Rent", "Buy"])
 
-salary = Salary(
-    monthly_salary=monthly_salary,
-    yearly_bonus=0,
-    increase_rate=salary_increase_rate,
-    years_until_retirement=year_until_retirement,
-    inflation_rate=inflation_rate
-)
-
-salaries = [salary.cashflow(year) for year in range(n)]
-salaries[0] += starting_balance
 
 with tabs[1]:
     st.markdown('## Rent')
-    monthtly_rent = st.number_input('Monthly rent (万円)', value=24, key='1')
-    renewal_fee = st.number_input('Renewal fee (万円)', value=24, key='4')
-
-    bank_rent = Bank(
-        initial_deposit=starting_balance,
-        interest_rate=market_rate,
-        inflation_rate=inflation_rate
-    )
+    monthtly_rent = st.number_input('Monthly rent (万円)', value=24)
+    renewal_fee = st.number_input('Renewal fee (万円)', value=24)
+    rent_increase_rate = st.number_input('Yearly rent increase', value=0.01)
     rent = Rent(
-        monthly_rent=monthtly_rent, 
+        monthly_rent=monthtly_rent,
         renewal_fee=renewal_fee,
-        rent_increase_rate=rent_increase_rate,
-        inflation_rate=inflation_rate
+        rent_increase_rate=rent_increase_rate
     )
-    data = []
-    for year in range(n):
-        cashflow = rent.cashflow(year) + salary.cashflow(year)
-        bank_rent.add_cash(cashflow)
-        assets = bank_rent.value_at_year(year)
-        data.append({
-            'year': year,
-            'cashflow': cashflow,
-            'value': assets,
-            'salaries': np.sum(salaries[:year + 1]),
-            'source': 'Rent'
-        })
-    
-    st.subheader("Cashflow and value table")
-    rent_data = pd.DataFrame(data)
-    st.dataframe(rent_data, use_container_width=True, hide_index=True)
-    
-    
-    
+    rent_bank = Bank(initial_deposit=0, interest_rate=market_rate)
+
+
 
 with tabs[2]:
     col1, col2 = st.columns(2)
     st.subheader("Property details")
-    house_price = st.number_input(f'House price (万円)', value=3500, key=7)
-    land_price = st.number_input('Land price (万円)', value=7500, key=8)
-    current_age = st.number_input(f'Current age of the house', value=0, key=72)
-    max_age = st.number_input(f'Fully amortized age of the house', value=22, key=73, help='Different property type can have different amortization period. For example, a wooden house will be amortized over 22 years.')
-    broker_fee = st.number_input('Broker fee', format='%f', value=0.07, key=9)
+    house_price = st.number_input('House price (万円)', value=3500)
+    land_price = st.number_input('Land price (万円)', value=7500)
+    current_age = st.number_input('Current age of the house', value=0)
+    max_age = st.number_input(
+        'Amortization period',
+        value=22,
+        help='Different property type can have different amortization period. For example, a wooden house will be amortized over 22 years.'
+    )
+    broker_fee = st.number_input('Broker fee', format='%f', value=0.07)
+    include_maintainance_cost = st.checkbox(
+        "Discount maintanance cost when selling", 
+        value=False, 
+        help="Whether or not to devalue by the maintenance cost that is due every ~15 years (roof, paint, etc) when selling the house."
+    )
 
     st.subheader('Loan details')
-    principal2 = st.number_input('Mortgage amount (万円)', value=int(0.9 * (house_price + land_price)), key=11)
-    mortgage_rate2 = st.number_input('Mortgage rate', format='%f', value=0.01, key=92)
-    mortage_period2 = st.number_input('Mortgage period', value=30, key=12)
+    principal = st.number_input('Mortgage amount (万円)', value=int(0.9 * (house_price + land_price)), key=11)
+    mortgage_rate = st.number_input('Mortgage rate', format='%f', value=0.01, key=92)
+    mortage_period = st.number_input('Mortgage period', value=30, key=12)
 
-    bank_buy = Bank(
-        initial_deposit=starting_balance, 
-        interest_rate=market_rate,
-        inflation_rate=inflation_rate
-    )
     house = RealEstate(
         house_value=house_price,
         house_age=current_age,
         fully_amortized_age=max_age,
         land_value=land_price,
-        down_payment=(house_price + land_price) * (1 + broker_fee) - principal2,
+        down_payment=(house_price + land_price) * (1 + broker_fee) - principal,
         broker_fee=broker_fee,
-        market_rate=appreciation_rate,
-        inflation_rate=inflation_rate
+        market_rate=appreciation_rate
     )
     loan = Loan(
-        principal=principal2,
-        yearly_interest=mortgage_rate2,
-        term=mortage_period2,
-        inflation_rate=inflation_rate
+        principal=principal,
+        yearly_interest=mortgage_rate,
+        term=mortage_period
     )
-    data = []
-    for year in range(n):
-        cashflow = house.cashflow(year) + loan.cashflow(year) + salary.cashflow(year)
-        bank_buy.add_cash(cashflow)
-        assets = house.value_at_year(year) + bank_buy.value_at_year(year)
-        liabilities = loan.value_at_year(year)
-        
-        data.append({
-            'year': year,
-            'cashflow': cashflow,
-            'value': assets + liabilities,
-            'salaries': np.sum(salaries[:year + 1]),
-            'source': 'Buy'
-        })
+    buy_bank = Bank(initial_deposit=0, interest_rate=market_rate)
 
+
+# Initialize cashflows
+for year in range(n + 1):
+    
+    rent_cashflow = rent.cashflow(year)
+    buy_cashflow = house.cashflow(year) + loan.cashflow(year)
+    
+    # Rent
+    amount_invested_in_stocks = max(0, abs(buy_cashflow) - abs(rent_cashflow))
+    rent_bank.add_cash(amount_invested_in_stocks)
+
+    # Buy
+    amount_invested_in_stocks = max(0, abs(rent_cashflow) - abs(buy_cashflow))
+    buy_bank.add_cash(amount_invested_in_stocks)
+
+# Compute value
+rdata, bdata = [], []
+for year in range(n + 1):
+    # Rent
+    npv = sum(rent.cashflow(y, discount_rate=inflation_rate) for y in range(year + 1))
+    if invest_difference:
+        npv += rent_bank.value_at_year(year, discount_rate=inflation_rate)
+    rdata.append({
+        'year': year,
+        'total_cost': rent.cashflow(year),
+        'rent_cost': rent.cashflow(year),
+        'loan_cost': 0,
+        'house_cost': 0,
+        'loan_value': 0,
+        'house_value': 0,
+        'bank_cashflow': rent_bank.cashflow(year),
+        'npv': npv,
+        'invested': rent_bank.cashflow(year),
+        'source': 'Rent'
+    })
+    
+    # Buy
+    npv = loan.value_at_year(year) + house.value_at_year(year, max_year=n, include_maintainance_cost=include_maintainance_cost)
+    for y in range(year + 1):
+        npv += loan.cashflow(y, discount_rate=inflation_rate)
+        npv += house.cashflow(y, discount_rate=inflation_rate)
+    if invest_difference:
+        npv += buy_bank.value_at_year(year, discount_rate=inflation_rate)
+    bdata.append({
+        'year': year,
+        'total_cost': loan.cashflow(year) + house.cashflow(year),
+        'rent_cost': 0,
+        'loan_cost': loan.cashflow(year),
+        'house_cost': house.cashflow(year),
+        'loan_value': loan.value_at_year(year),
+        'house_value': house.value_at_year(year),
+        'bank_cashflow': buy_bank.cashflow(year),
+        'npv': npv,        
+        'source': 'Buy'
+    })
+
+
+with tabs[1]:
     st.subheader("Cashflow and value table")
-    buy1_data = pd.DataFrame(data)
-    st.dataframe(buy1_data, use_container_width=True, hide_index=True)
+    rent_data = pd.DataFrame(rdata)
+    st.dataframe(rent_data, use_container_width=True, hide_index=True)
+
+with tabs[2]:
+    st.subheader("Cashflow and value table")
+    buy_data = pd.DataFrame(bdata)
+    st.dataframe(buy_data, use_container_width=True, hide_index=True)
 
 
 with tabs[0]:
     st.subheader("Rent Or Buy")
 
-    
-    data = pd.concat([rent_data, buy1_data])
-    fig = px.bar(data, x="year", y="cashflow", color="source", barmode='group', title="Cash to invest (万円)")
+    data = pd.concat([rent_data, buy_data])
+    fig = px.bar(data, x="year", y="total_cost", color="source", barmode='group', title="Project cost (nominal 万円)")
     st.plotly_chart(fig, use_container_width=True)
 
-    nominal_or_real = 'Real' if inflation_rate > 0 else 'Nominal'
-    fig = px.line(data, x="year", y="value", color="source", title=f"{nominal_or_real} Net Worth (万円)")
-    st.plotly_chart(fig, use_container_width=True)
-
+    fig = px.line(data, x="year", y="npv", color="source", title=f"Project Net Present Value (real 万円)")
+    st.plotly_chart(fig, use_container_width=True)  
--- models.py
+++ models.py
@@ -14,79 +14,87 @@ class RealEstate:
             fully_amortized_age,
             land_value, 
             down_payment=0, 
-            property_type='HOUSE',
+            house_surface=100,
             broker_fee=0.07, 
-            market_rate=0.02,
-            inflation_rate=0
+            market_rate=0.02
         ):
         self.house_value = house_value
         self.house_age = house_age
+        self.house_surface = house_surface
         self.fully_amortized_age = fully_amortized_age
         self.land_value = land_value
         self.down_payment = down_payment
-        self.property_type = property_type
         self.broker_fee = broker_fee
         self.market_rate = market_rate
-        self.inflation_rate = inflation_rate
 
     @property
     def total_cost(self):
         return (self.house_value + self.land_value) * (1 + self.broker_fee)
 
-
-    def value_at_year(self, n):
+    def _value_at_year(self, n, discount_rate=0.0):
         value = self.land_value + self.house_value * (1 - min(1, (n + self.house_age) / self.fully_amortized_age))
-        value = (1 - self.broker_fee) * value * (1 + self.market_rate)**n 
-        return value / (1 + self.inflation_rate)**n
+        value *= (1 - self.broker_fee) * (1 + self.market_rate)**n
+        return value / (1 + discount_rate)**n
 
-    def cashflow(self, year):
-        return -self.down_payment if year == 0 else 0
+    def value_at_year(self, year, discount_rate=0.0, max_year=50, include_maintainance_cost=False):
+        value = self._value_at_year(year, discount_rate=discount_rate)
+        if include_maintainance_cost:
+            for y in range(year, max_year + 1):
+                value += self.cashflow(y, discount_rate=discount_rate)
+        return value
+
+    def cashflow(self, year, discount_rate=0.0):
+        flow = -self._value_at_year(year) * (.04 / 6 *  + .03 / 3)
+        if year == 0:
+            flow -= self.down_payment
+        elif (year % 15) == 0:
+            flow -= 500 * self.house_surface / 100
+        return flow / (1 + discount_rate)**year
 
 
 class Bank:
 
-    def __init__(self, initial_deposit, interest_rate=0, inflation_rate=0):
+    def __init__(self, initial_deposit, interest_rate=0):
         self.initial_deposit = initial_deposit
         self.interest_rate = interest_rate
         self.cashflows = []
-        self.inflation_rate = inflation_rate
 
     def add_cash(self, amount):
         self.cashflows.append(amount)
 
-    def value_at_year(self, year):
+    def value_at_year(self, year, discount_rate=0.0):
         value = self.initial_deposit
-        for cf in self.cashflows[:year+1]:
+        for cf in self.cashflows[:year + 1]:
             value += cf
             if value > 0:
                 value *= (1 + self.interest_rate)
-        return value / (1 + self.inflation_rate)**year
+        return value / (1 + discount_rate)**year
 
-    def cashflow(self, year):
-        return 0
+    def cashflow(self, year, discount_rate=0.0):
+        return self.cashflows[year]
 
 
 class Rent:
 
-    def __init__(self, monthly_rent, renewal_fee, rent_increase_rate, inflation_rate=0):
+    def __init__(self, monthly_rent, renewal_fee, rent_increase_rate):
         self.monthly_rent = monthly_rent
-        self.inflation_rate = inflation_rate
         self.renewal_fee = renewal_fee
         self.rent_increase_rate = rent_increase_rate
 
-    def cashflow(self, year):
-        value = self.monthly_rent * 12 
+    def cashflow(self, year, discount_rate=0.0):
+        flow = -self.monthly_rent * 12 
         if year % 2 == 0:
-            value += self.renewal_fee
-        return -value * (1 + self.rent_increase_rate)**year
+            flow -= self.renewal_fee
+        flow *= (1 + self.rent_increase_rate)**year
+        return flow / (1 + discount_rate)**year
 
-    def value_at_year(self, year):
+    def value_at_year(self, year, discount_rate=0.0):
         return 0
 
 
 class Loan:
 
-    def __init__(self, principal, yearly_interest, term, inflation_rate=0):
+    def __init__(self, principal, yearly_interest, term):
         self.loan = mortgage.Loan(
             principal=principal,
             interest=yearly_interest,
@@ -96,31 +104,29 @@ class Loan:
         self.principal = principal
         self.interest = yearly_interest
         self.term = term
-        self.inflation_rate = inflation_rate
 
-    def cashflow(self, year):
-        value = -self.monthly_payment * 12 if year < self.term else 0
-        return value
+    def cashflow(self, year, discount_rate=0.0):
+        flow = -self.monthly_payment * 12 if year < self.term else 0
+        return flow / (1 + discount_rate)**year
 
-    def value_at_year(self, year):
-        value =  -(float(self.loan.total_paid) - self.monthly_payment * 12 * (year + 1))
-        return min(value, 0) / (1 + self.inflation_rate)**year
-
+    def value_at_year(self, year, discount_rate=0.0):
+        value =  -(float(self.loan.total_paid) - self.monthly_payment * 12 * year)
+        return min(value, 0) / (1 + discount_rate)**year
 
 class Salary:
 
-    def __init__(self, monthly_salary, yearly_bonus=0, years_until_retirement=30, increase_rate=0, inflation_rate=0):
+    def __init__(self, monthly_salary, yearly_bonus=0, years_until_retirement=30, increase_rate=0):
         self.monthly_salary = monthly_salary
         self.yearly_bonus = yearly_bonus
         self.increase_rate = increase_rate
-        self.inflation_rate = inflation_rate
         self.years_until_retirement = years_until_retirement
 
-    def cashflow(self, year):
+    def cashflow(self, year, discount_rate=0.0):
         if year >= self.years_until_retirement:
             return 0
-        value = self.monthly_salary * 12 + self.yearly_bonus 
-        return value * (1 + self.increase_rate)**year
+        flow = self.monthly_salary * 12 + self.yearly_bonus 
+        flow *= (1 + self.increase_rate)**year
+        return flow / (1 + discount_rate)**year
 
 
 if __name__ == '__main__':
