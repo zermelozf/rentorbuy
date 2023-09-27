5f8259b95c68ac67ba38ea889efbad2f78763539
Arnaud Rachez <arnaud@linalgo.com>
2023-09-27 16:45:47 +0900

    feat: simplify and use relative NPV

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from collections import defaultdict
-
 import streamlit as st
+import numpy_financial as npf
 import numpy as np
 import pandas as pd
 
 import plotly.express as px
+import plotly.graph_objects as go
 
-from models import Bank, Loan, RealEstate, Rent, Salary
+
+from models import Bank, Loan, RealEstate, Rent
 
 
 n = st.sidebar.slider("Timeline (years)", value=35, max_value=50)
@@ -24,11 +25,11 @@ inflation_rate = st.sidebar.number_input(
     format='%f',
     help='Used to discount the future value of future payments. It will affect the real NPV of the project.'
 )
-invest_difference = st.sidebar.checkbox(
-    "Invest difference in the stock market", 
-    value=False, 
-    help="Each year one project has a cost bigger than the other, the second project will invest the difference in the stock market."
-)
+invest_difference = False  # st.sidebar.checkbox(
+#     "Invest difference in the stock market", 
+#     value=False, 
+#     help="If the monthly cost of renting is cheaper than that of buying, you can invest the difference in stocks. Same thing if it is the other way around."
+# )
 market_rate = 0
 if invest_difference:
     market_rate = st.sidebar.number_input(
@@ -38,6 +39,10 @@ if invest_difference:
         help='This return is used for inveting any cash left after paying the rent and/or loan into the stock market.'
     )
 
+discount_rate = inflation_rate
+if invest_difference:
+    discount_rate = (1 + inflation_rate) * (1 + market_rate) - 1
+
 tabs = st.tabs(["Summary", "Rent", "Buy"])
 
 
@@ -46,12 +51,12 @@ with tabs[1]:
     monthtly_rent = st.number_input('Monthly rent (万円)', value=24)
     renewal_fee = st.number_input('Renewal fee (万円)', value=24)
     rent_increase_rate = st.number_input('Yearly rent increase', value=0.01)
+    
     rent = Rent(
         monthly_rent=monthtly_rent,
         renewal_fee=renewal_fee,
         rent_increase_rate=rent_increase_rate
     )
-    rent_bank = Bank(initial_deposit=0, interest_rate=market_rate)
 
 
 
@@ -68,15 +73,10 @@ with tabs[2]:
     )
     broker_fee = st.number_input('Broker fee', format='%f', value=0.07)
     include_maintainance_cost = st.checkbox(
-        "Discount maintanance cost when selling", 
-        value=False, 
+        "Discount maintenance cost when selling", 
+        value=True, 
         help="Whether or not to devalue by the maintenance cost that is due every ~15 years (roof, paint, etc) when selling the house."
     )
-
-    st.subheader('Loan details')
-    principal = st.number_input('Mortgage amount (万円)', value=int(0.9 * (house_price + land_price)), key=11)
-    mortgage_rate = st.number_input('Mortgage rate', format='%f', value=0.01, key=92)
-    mortage_period = st.number_input('Mortgage period', value=30, key=12)
 
     house = RealEstate(
         house_value=house_price,
@@ -86,88 +86,62 @@ with tabs[2]:
         broker_fee=broker_fee,
         market_rate=appreciation_rate
     )
+
+    st.subheader('Loan details')
+    principal = st.number_input('Mortgage amount (%)', value=0.9) * house.buy_cost()
+    mortgage_rate = st.number_input('Mortgage rate', format='%f', value=0.01)
+    mortage_period = st.number_input('Mortgage period', value=30)
+
+    
     loan = Loan(
         principal=principal,
         yearly_interest=mortgage_rate,
         term=mortage_period,
         first_payment=house.buy_cost() - principal
     )
-    buy_bank = Bank(initial_deposit=0, interest_rate=market_rate)
 
 
 # Initialize cashflows
+data = {
+    'year': [],
+    'saved_rent': [],
+    'cashflow': [],
+    'npv': [],
+    'irr': []
+}
 for year in range(n + 1):
-    
-    rent_cashflow = rent.cashflow(year)
-    buy_cashflow = house.cashflow(year) + loan.cashflow(year)
-    
-    # Rent
-    amount_invested_in_stocks = max(0, abs(buy_cashflow) - abs(rent_cashflow))
-    rent_bank.add_cash(amount_invested_in_stocks)
-
-    # Buy
-    amount_invested_in_stocks = max(0, abs(rent_cashflow) - abs(buy_cashflow))
-    buy_bank.add_cash(amount_invested_in_stocks)
-
-# Compute value
-rdata, bdata = [], []
-for year in range(n + 1):
-    # Rent
-    npv = sum(rent.cashflow(y, discount_rate=inflation_rate) for y in range(year + 1))
-    if invest_difference:
-        npv += rent_bank.value_at_year(year, discount_rate=inflation_rate)
-    rdata.append({
-        'year': year,
-        'total_cost': rent.cashflow(year),
-        'rent_cost': rent.cashflow(year),
-        'loan_cost': 0,
-        'house_cost': 0,
-        'loan_value': 0,
-        'house_value': 0,
-        'bank_cashflow': rent_bank.cashflow(year),
-        'npv': npv,
-        'invested': rent_bank.cashflow(year),
-        'source': 'Rent'
-    })
-    
-    # Buy
-    npv = loan.value_at_year(year) + house.value_at_year(year, max_year=n, include_maintainance_cost=include_maintainance_cost)
-    for y in range(year + 1):
-        npv += loan.cashflow(y, discount_rate=inflation_rate)
-        npv += house.cashflow(y, discount_rate=inflation_rate)
-    if invest_difference:
-        npv += buy_bank.value_at_year(year, discount_rate=inflation_rate)
-    bdata.append({
-        'year': year,
-        'total_cost': loan.cashflow(year) + house.cashflow(year),
-        'rent_cost': 0,
-        'loan_cost': loan.cashflow(year),
-        'house_cost': house.cashflow(year),
-        'loan_value': loan.value_at_year(year),
-        'house_value': house.value_at_year(year),
-        'bank_cashflow': buy_bank.cashflow(year),
-        'npv': npv,        
-        'source': 'Buy'
-    })
-
-
-with tabs[1]:
-    st.subheader("Cashflow and value table")
-    rent_data = pd.DataFrame(rdata)
-    st.dataframe(rent_data, use_container_width=True, hide_index=True)
-
-with tabs[2]:
-    st.subheader("Cashflow and value table")
-    buy_data = pd.DataFrame(bdata)
-    st.dataframe(buy_data, use_container_width=True, hide_index=True)
+    data['year'].append(year)
+    data['saved_rent'].append(rent.cashflow(year))
+    data['cashflow'].append(house.cashflow(year) + loan.cashflow(year))
+    cashflow_with_sale = np.array(data['cashflow']) - np.array(data['saved_rent'])
+    sale_money = (
+        house.value_at_year(year, max_year=n, include_maintainance_cost=include_maintainance_cost) + 
+        loan.value_at_year(year, discount_rate=discount_rate)
+    )
+    cashflow_with_sale[-1] += sale_money
+    data['npv'].append(npf.npv(discount_rate, cashflow_with_sale))
+    data['irr'].append(round(npf.irr(cashflow_with_sale), year))
+data = pd.DataFrame(data)
 
 
 with tabs[0]:
     st.subheader("Rent Or Buy")
 
-    data = pd.concat([rent_data, buy_data])
-    fig = px.bar(data, x="year", y="total_cost", color="source", barmode='group', title="Project cost (nominal 万円)")
+    for y, v in enumerate(data['npv']):
+        if v >= 0:
+            break
+    st.info(f"Buying a house is more interesting if you intend to :orange[**stay for more than {y} years**]. For shorter periods, renting is more interesting financially.", icon="ℹ️")
+    
+    fig = px.line(data, x="year", y="npv", title=f"Net Present Value (real 万円)")
     st.plotly_chart(fig, use_container_width=True)
 
-    fig = px.line(data, x="year", y="npv", color="source", title=f"Project Net Present Value (real 万円)")
-    st.plotly_chart(fig, use_container_width=True)  
+    st.info(f"Your :orange[**loan will cost {int(loan.monthly_payment * 10000):,} JPY/month**] or {int(loan.monthly_payment * 12 * 10000):,} JPY/year. You will also need to pay for maintenance, and taxes for your home.", icon="ℹ️")
+    
+    fig = px.bar(data, x="year", y="cashflow", title="Yearly Cashflow (nominal 万円)")
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.info(f"In order to maximize your investment's rate of return, you should :orange[**sell after {np.argmax(data['irr'])} years.**]", icon="ℹ️")
+
+    fig = px.line(data, x="year", y="irr", title="Internal Rate of Return")
+    st.plotly_chart(fig, use_container_width=True)
+
